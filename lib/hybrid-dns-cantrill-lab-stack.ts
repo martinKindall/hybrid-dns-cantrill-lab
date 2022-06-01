df0f134b96ddf17263fa0e44ac2684f34358247f
Martin <mart256@gmail.com>
2022-06-01 22:36:09 +0200

    Configuring the vpc and the subnets with the table

--- lib/hybrid-dns-cantrill-lab-stack.ts
+++ lib/hybrid-dns-cantrill-lab-stack.ts
@@ -1,19 +1,48 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, Tags } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class HybridDnsCantrillLabStack extends Stack {
+  private vpc: ec2.Vpc;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'HybridDnsCantrillLabQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    this.vpc = new ec2.Vpc(this, 'onprem_vpc', {
+      cidr: '192.168.10.0/24',
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
+      subnetConfiguration: [],
+    });
+    Tags.of(this.vpc).add('Name', 'a4l-onprem');
+
+    const priv1Subnet = new ec2.CfnSubnet(this, 'priv1Subnet', {
+      vpcId: this.vpc.vpcId,
+      availabilityZone: this.availabilityZones[0],
+      cidrBlock: '192.168.10.0/25',
+      tags: [{key: 'Name', value: 'sn-onprem-A'}]
     });
 
-    const topic = new sns.Topic(this, 'HybridDnsCantrillLabTopic');
+    const priv2Subnet = new ec2.CfnSubnet(this, 'priv2Subnet', {
+      vpcId: this.vpc.vpcId,
+      availabilityZone: this.availabilityZones[1],
+      cidrBlock: '192.168.10.128/25',
+      tags: [{key: 'Name', value: 'sn-onprem-B'}]
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const privRT = new ec2.CfnRouteTable(this, 'privRT', {
+      vpcId: this.vpc.vpcId,
+      tags: [{key: 'Name', value: 'A4L-ONPREM-RT'}]
+    });
+
+    const rtToPriv1Association = new ec2.CfnSubnetRouteTableAssociation(this, 'rtToPriv1Association', {
+      subnetId: priv1Subnet.attrSubnetId,
+      routeTableId: privRT.attrRouteTableId
+    });
+
+    const rtToPriv2Association = new ec2.CfnSubnetRouteTableAssociation(this, 'rtToPriv2Association', {
+      subnetId: priv2Subnet.attrSubnetId,
+      routeTableId: privRT.attrRouteTableId
+    });
   }
 }
